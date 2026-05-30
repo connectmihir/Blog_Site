0a874ab5af3bf08ffdc0e05aa04a61e9bb2c5aef
connectmihir <connectmihirr@gmail.com>
2026-05-30 21:48:57 +0530

    keynote

--- database.php
+++ database.php
@@ -1,5 +1,20 @@
-<?php
+<!-- Steps to integrate database:
+1. Create neccessary varibles to store information about database.
 
+2. create another variable which stores database connection
+
+
+3. Use if and else condition to check connection. -->
+
+
+
+
+
+
+
+
+
+<?php
 use Dom\Sqlite;
 
 $server="localhost";
