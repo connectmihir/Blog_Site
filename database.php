87216c891d01f3c862301aa51f79d829402eed79
connectmihir <connectmihirr@gmail.com>
2026-05-30 13:59:20 +0530

    update

--- database.php
+++ database.php
@@ -1,7 +1,19 @@
 <?php
 
+use Dom\Sqlite;
+
 $server="localhost";
 $user="root";
 $password="";
 $dbname= "blog_site";
+
+$connection =new mysqli($server,$user, $password, $dbname);
+
+if(!$connection){
+    echo"Error!!: {$connection->connect_error}";
+}
+
+else{
+    echo"Successfully connected to the database!!!!";
+}
 ?>
