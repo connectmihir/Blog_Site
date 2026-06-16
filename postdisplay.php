3f0f0c6ed12af15dbce32f32eeb3ba383129c223
connectmihir <connectmihirr@gmail.com>
2026-06-16 23:40:20 +0530

    display Post

--- postdisplay.php
+++ postdisplay.php
@@ -6,14 +6,14 @@ session_start();
 
 include "database.php";
 
-$sql= "select * from posts";
+$sql= "select * from post";
 
 $Result= mysqli_query($connection, $sql);
 
 while($row= mysqli_fetch_assoc($Result)){
-    echo"{$row['Titile']}<br>";
+    echo"{$row['title']}<br>";
     echo"{$row['content']}<br>";
-    echo"<img src="/Blog_Site/Image/{$row['image']}" alt=""><br><hr>";
+    echo"<img src='Image/{$row['image']}'><br><hr>";
 }
 
 
