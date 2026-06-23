1f3fff4389e1523e1d86cd07a24fd52cda408e96
connectmihir <connectmihirr@gmail.com>
2026-06-23 21:25:35 +0530

    updation system

--- postdisplay.php
+++ postdisplay.php
@@ -14,6 +14,11 @@ while($row= mysqli_fetch_assoc($Result)){
     echo"{$row['title']}<br>";
     echo"{$row['content']}<br>";
     echo"<img src='Image/{$row['image']}'><br><hr>";
+
+    echo"<a href='updatepost.php?post_id={$row['id']}'>Update the post!!</a> <br>";
+    echo"<a href='deletepost.php'>Delete the post!!</a> <br>";
+    // echo""<a href="">Delete the post!!</a> <br>
+
 }
 
 
