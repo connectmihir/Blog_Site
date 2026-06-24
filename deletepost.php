3695e87934a79c47a6b98fc01431241313996383
connectmihir <connectmihirr@gmail.com>
2026-06-24 10:32:21 +0530

    Working on  comment system

--- deletepost.php
+++ deletepost.php
@@ -5,15 +5,27 @@ session_start();
 
 include "database.php";
 
-if (isset($_GET['post_id'])) {
-    $post_id = $_GET['post_id'];
-}
+if (!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    exit();
+} 
 
-$sql = "delete from post where id='$post_id'";
-$result = mysqli_query($connection, $sql);
+else {
 
-if (!$result) {
-    die("Connection failed: " . $connection->connect_error);
-} else {
-    echo "deleted SuccessFully";
-}
+        if (isset($_GET['post_id'])) {
+            $post_id = $_GET['post_id'];
+        }
+
+        $sql = "delete from post where id='$post_id'";
+        $result = mysqli_query($connection, $sql);
+
+        if (!$result) {
+            die("Connection failed: " . $connection->connect_error);
+        } 
+        
+        else {
+            echo "deleted SuccessFully";
+        }
+    }
+
+?>
--- postdisplay.php
+++ postdisplay.php
@@ -23,12 +23,7 @@ while($row= mysqli_fetch_assoc($Result)){
     echo"{$row['title']}<br>";
     echo"{$row['content']}<br>";
     echo"<img src='Image/{$row['image']}'><br><hr>";
-
-    echo"<a href='updatepost.php?post_id={$row['id']}'>Update the post!!</a> <br>";
-    echo"<a href='deletepost.php?post_id={$row['id']}'>Delete the post!!</a> <br>";
-    
-
-}
+ }
 
 
 
@@ -44,6 +39,23 @@ while($row= mysqli_fetch_assoc($Result)){
 <body>
 
 <form action="postdisplay.php?user_name=$user_name">
+
+<input type="hidden" name="post_id" value="<?php echo $row['id']; ?>">
+    <?php if (in_array($_SESSION['user_role'], ['author', 'admin'])) { ?>
+
+        <a href="updatepost.php?post_id=<?php echo $row['id']; ?>">
+            Update the post
+        </a>
+
+        <br>
+
+        <a href="deletepost.php?post_id=<?php echo $row['id']; ?>">
+            Delete the post
+        </a>
+
+        <br><br>
+
+    <?php } ?>
 
 
 <textarea name="comment" placeholder="We love to hear you and learn from you!!">
