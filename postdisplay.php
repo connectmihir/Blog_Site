0b3712e521120fd883170833aecd377395b689f4
connectmihir <connectmihirr@gmail.com>
2026-06-24 09:19:25 +0530

    Working on  comment system

--- postdisplay.php
+++ postdisplay.php
@@ -17,10 +17,37 @@ while($row= mysqli_fetch_assoc($Result)){
 
     echo"<a href='updatepost.php?post_id={$row['id']}'>Update the post!!</a> <br>";
     echo"<a href='deletepost.php?post_id={$row['id']}'>Delete the post!!</a> <br>";
-    // echo""<a href="">Delete the post!!</a> <br>
+    
 
 }
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogs</title>
+</head>
+<body>
+
+<form action="postdisplay.php">
+
+
+<textarea name="comment" placeholder="We love to hear you and learn from you!!">
+
+</textarea>
+
+<button id="login" 
+        name="insert_comment" >Insert Comment</button>
+
+
+
+
+</form>
+    
+</body>
+</html>
